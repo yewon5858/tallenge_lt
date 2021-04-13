452b83224b6fe6cdb09845e1e90e0f07aa3edb1d
Kimyewon <nerino5885@gmail.com>
2021-04-14 01:10:27 +0900

    <color name="tallengeblue"> #C9DAF8</color> 우리 배경색 입니다!!

--- app/src/main/java/com/example/tallenge_lt/LoginActivity.java
+++ app/src/main/java/com/example/tallenge_lt/LoginActivity.java
@@ -3,14 +3,12 @@ package com.example.tallenge_lt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
