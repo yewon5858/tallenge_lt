74a360b40489420540e16a7db7a48053f8d339d5
EonSeung99 <tjddjstmd5398@gmail.com>
2021-04-14 11:44:22 +0900

    add Activity and xml

---
+++ app/src/main/java/com/example/tallenge_lt/ChooseListActivity.java
@@ -0,0 +1,14 @@
+package com.example.tallenge_lt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch_list);
+    }
+}
