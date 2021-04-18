bc862b7096ecdd497f71e239a7122ae1de4a3afc
Kimyewon <nerino5885@gmail.com>
2021-04-19 04:35:51 +0900

    메인 카테고리 제외 완성

---
+++ app/src/main/java/com/example/tallenge_lt/ExpActivity.java
@@ -0,0 +1,14 @@
+package com.example.tallenge_lt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exp);
+    }
+}
