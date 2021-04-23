213deffb803fa267cbfc79708b3beb4b5fdce177
EonSeung99 <tjddjstmd5398@gmail.com>
2021-04-23 19:07:08 +0900

    4/23 +SplashActivity edit

--- app/src/main/java/com/example/tallenge_lt/ExecutionActivity.java
+++ app/src/main/java/com/example/tallenge_lt/ExecutionActivity.java
@@ -1,8 +1,9 @@
 package com.example.tallenge_lt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
 public class ExecutionActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class ExecutionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exe);
+        //로딩화면 시작.
+        Loadingstart();
+    }
+    private void Loadingstart(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
