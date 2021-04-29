14780eda0ae2f74332f4747a88148bcea1c1645b
yubin <ydbqls13@naver.com>
2021-04-29 22:53:10 +0900

    4/29

--- app/src/main/java/com/example/tallenge_lt/ChatListActivity.java
+++ app/src/main/java/com/example/tallenge_lt/ChatListActivity.java
@@ -1,8 +1,8 @@
 package com.example.tallenge_lt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ChatListActivity extends AppCompatActivity {
 
@@ -10,5 +10,7 @@ public class ChatListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatlist);
+
     }
+
 }
