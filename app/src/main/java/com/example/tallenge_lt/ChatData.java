229812fd0d3bcd2847bfd9956a1157c03f2b1245
EonSeung99 <tjddjstmd5398@gmail.com>
2021-05-05 03:21:25 +0900

    5/5 채팅기능 구현(USER 미연동)

---
+++ app/src/main/java/com/example/tallenge_lt/ChatData.java
@@ -0,0 +1,23 @@
+package com.example.tallenge_lt;
+
+public class ChatData {
+
+    private String msg;
+    private String nickname;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
