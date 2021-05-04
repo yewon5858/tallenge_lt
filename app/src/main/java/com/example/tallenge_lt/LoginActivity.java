d0df5097c5fff7094cbd76112f70db068ffa4ba4
Kimyewon <nerino5885@gmail.com>
2021-05-05 01:14:51 +0900

    회원가입에서 nicname 추가하는거 해보려고했는데 안되서 추가로 회원 정보 입력따로 만들어보겠씁니다!!

--- app/src/main/java/com/example/tallenge_lt/LoginActivity.java
+++ app/src/main/java/com/example/tallenge_lt/LoginActivity.java
@@ -2,22 +2,79 @@ package com.example.tallenge_lt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    Button bt_login;
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
+    private DatabaseReference mDatabaseRef;
+    private EditText mEtEmail, mEtPwd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        bt_login = (Button) findViewById(R.id.bt_login);
-        bt_login.setOnClickListener((v) ->{
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-        });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("tallenge");
+
+        mEtEmail = findViewById( R.id.et_email );
+        mEtPwd = findViewById( R.id.et_pwd );
+
+        Button btn_login = findViewById( R.id.btn_login );
+        btn_login.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //로그인 요청
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                //Firebase Auth 진행
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener( LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //로그인 성공
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity( intent );
+                            finish(); // 현재 액티비티 파괴
+                            Toast.makeText(LoginActivity.this,"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                        } else{
+                            //로그인 실패
+                            Toast.makeText(LoginActivity.this,"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } );
+            }
+        } );
+
+        Button btn_register = findViewById( R.id.btn_register );
+        btn_register.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 회원가입 화면으로 이동
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity( intent );
+            }
+        } );
+
+        };
     }
 
-}
+
+
+
+
+
