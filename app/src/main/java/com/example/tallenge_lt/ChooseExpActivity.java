1303b6d45f77d220c502a75d3f828ec2ec8959fc
yubin <ydbqls13@naver.com>
2021-05-12 18:50:17 +0900

    수정

--- app/src/main/java/com/example/tallenge_lt/ChooseExpActivity.java
+++ app/src/main/java/com/example/tallenge_lt/ChooseExpActivity.java
@@ -41,7 +41,7 @@ public class ChooseExpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ch_exp);
 
         recyclerView = (RecyclerView)findViewById(R.id.ch_exp_rv);
-        recyclerView.setHasFixedSize(true);  //리사이클러뷰 성능 강화
+        //recyclerView.setHasFixedSize(true);  //리사이클러뷰 성능 강화
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -49,18 +49,18 @@ public class ChooseExpActivity extends AppCompatActivity {
 
 
         database = FirebaseDatabase.getInstance();  //파이어베이스 데이터베이스 연동
-        databaseReference = database.getReference("tallenge");   //DB테이블 연결
+        databaseReference = database.getReference("tallenge").child("exp");   //DB테이블 연결
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 //파이어베이스 데이터베이스의 데이터를 받아오는 곳
                 arrayList.clear();  //기존 배열리스트가 존재하지 않게 초기화
                 for(DataSnapshot snapshot1 : snapshot.getChildren()) {   //반복문으로 데이터 리스트 추출
+                    //오류있음
                     ChooseExpData chooseExpData = snapshot1.getValue(ChooseExpData.class);// 만들어뒀던 객체에 데이터를 담는다.
                     arrayList.add(chooseExpData);  //담은 데이터를 배열리스트에 넣고 리사이클러뷰로 보낼 준비
                 }
                 adapter.notifyDataSetChanged();   //리스트 저장 및 새로고침
-
             }
 
             @Override
--- app/src/main/java/com/example/tallenge_lt/SetAlarmActivity.java
+++ app/src/main/java/com/example/tallenge_lt/SetAlarmActivity.java
@@ -1,14 +1,31 @@
 package com.example.tallenge_lt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SetAlarmActivity extends AppCompatActivity {
 
+    CheckBox checkBox;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setalarm);
+
+        checkBox = findViewById(R.id.checkBox);
+        button = findViewById(R.id.btn_set_alarm);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBox.isChecked()){
+
+                }
+            }
+        });
     }
 }
