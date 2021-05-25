e94a0c12b4a8a2ce97848b55e9728bbd70a358d3
yubin <ydbqls13@naver.com>
2021-05-26 03:17:51 +0900

    05/16

--- app/src/main/java/com/example/tallenge_lt/MyInfoActivity.java
+++ app/src/main/java/com/example/tallenge_lt/MyInfoActivity.java
@@ -5,8 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MyInfoActivity extends AppCompatActivity {
     ImageButton bt_home;
@@ -14,7 +24,15 @@ public class MyInfoActivity extends AppCompatActivity {
     ImageButton  bt_alarm;
     ImageButton bt_mypage;
     Button btn_back, btn_exp, btn_checklist, btn_interest, btn_modifyinfo;
+    TextView textView;
 
+    FirebaseDatabase firebaseDatabase= FirebaseDatabase.getInstance();;
+
+    DatabaseReference databaseRef;
+
+    String emailId;
+    String IdToken;
+    String user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +43,13 @@ public class MyInfoActivity extends AppCompatActivity {
         btn_checklist = (Button)findViewById(R.id.btn_go_checklist);
         btn_interest = (Button)findViewById(R.id.btn_go_itr);
         btn_modifyinfo = (Button)findViewById(R.id.btn_go_modifyinfo);
+
+
+        emailId = getIntent().getStringExtra("emailId");
+        IdToken = getIntent().getStringExtra("idToken");
+        user = getIntent().getStringExtra("user");
+
+
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +83,9 @@ public class MyInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MyInfoActivity.this, ModifyInfoActivity.class) ;
+                intent.putExtra( "idToken",IdToken );
+                intent.putExtra( "emailId",emailId);
+                intent.putExtra(  "user",user);
                 startActivity( intent );
             }
         });
@@ -95,6 +123,24 @@ public class MyInfoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent( getApplicationContext(), MyInfoActivity.class );
                 startActivity( intent );
+            }
+        });
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        databaseRef= firebaseDatabase.getReference("tallenge").child("UserAccount").child(user.getUid());
+        textView = (TextView)findViewById(R.id.id);
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String data = dataSnapshot.child("emailId").getValue( String.class );
+                textView.setText(data);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
